13ccb03bba1d9150d0f19ae436b917fb2d27f92e
NguyenPT <nixforest21991920@gmail.com>
2018-04-18 17:25:41 +0700

    Handle add layout for Customer information view (front module)

--- protected/modules/front/views/customer/view.php
+++ protected/modules/front/views/customer/view.php
@@ -1,4 +1,374 @@
 <?php
 /* @var $this CustomerController */
 /* @var $model Customers */
-echo $model->name;
+//echo $model->name;
+?>
+<div class="maincontent-2 clearfix">
+    <div class="title-1">
+        <span class="icon10"></span> Hồ sơ bệnh án
+    </div>
+    <div class="profile__info">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="profile__item">
+                    <div class="profile__img">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tempt/img1.jpg" alt="">
+                    </div>
+                    <div class="profile__des">
+                        <div class="p-item">
+                            <span class="icon-p icon-11"></span>
+                            <span class="p-info">
+                                <strong>Bệnh Nhân:</strong> Phạm Hoàng Nam</span>
+                        </div>
+                        <div class="p-item">
+                            <span class="icon-p icon-12"></span>
+                            <span class="p-info">
+                                <strong>Ngày Sinh:</strong> 19/03/1991</span>
+                        </div>
+                        <div class="p-item">
+                            <span class="icon-p icon-13"></span>
+                            <span class="p-info">
+                                <strong>Giới Tính:</strong> Nam</span>
+                        </div>
+                        <div class="p-item">
+                            <span class="icon-p icon-14"></span>
+                            <span class="p-info">
+                                <strong>Mã Số Hồ Sơ:</strong>19870</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="title-1">
+                    <span class="icon15"></span> Thông tin liên lạc
+                </div>
+                <div class="p-add-info">
+                    <div class="p-item">
+                        <span class="icon-p icon-16"></span>
+                        <span class="p-info">
+                            <strong>Địa Chỉ:</strong> Số 1288 Huỳnh Tấn Phát, Phú Mỹ, Quận 7, TP Hồ Chí Minh</span>
+                    </div>
+                    <div class="p-item">
+                        <span class="icon-p icon-17"></span>
+                        <span class="p-info">
+                            <strong>Số Điện Thoại: </strong> 0981 819 143</span>
+                    </div>
+                    <div class="p-item">
+                        <span class="icon-p icon-18"></span>
+                        <span class="p-info">
+                            <strong>Email:</strong> namph@gmail.com </span>
+                        <a href="#">(Xem Thêm)</a>
+                    </div>
+                </div>
+                <div class="p-add-g clearfix">
+                    <div class="p-add-1">
+                        <div class="title-1">
+                            <span class="icon19"></span> Thông tin tiền sử
+                        </div>
+                        <div class="p-add-info">
+                            <div class="p-item">
+                                <span class="icon-p icon-20"></span>
+                                <span class="p-info">Cao Huyết Áp</span>
+                            </div>
+                            <div class="p-item">
+                                <span class="icon-p icon-20"></span>
+                                <span class="p-info">Sỏi Thận</span>
+                            </div>
+                            <div class="p-item">
+                                <span class="icon-p icon-20"></span>
+                                <span class="p-info">Viêm Gan Siêu Vi B </span>
+                            </div>
+                            <a href="#">(Xem Thêm)</a>
+                        </div>
+                    </div>
+                    <div class="p-add-1">
+                        <div class="title-1">
+                            <span class="icon21"></span> Thông tin liên quan
+                        </div>
+                        <div class="p-add-info">
+                            <div class="p-item">
+                                <span class="icon-p icon-22"></span>
+                                <span class="p-info">
+                                    <strong>Bác Sĩ Điều Trị:</strong> Đỗ Tấn Khoa</span>
+                            </div>
+                            <div class="p-item">
+                                <span class="icon-p icon-23"></span>
+                                <span class="p-info">
+                                    <strong>Kinh Doanh:</strong> Phạm Đức Trường</span>
+                            </div>
+                            <div class="p-item">
+                                <span class="icon-p icon-24"></span>
+                                <span class="p-info">
+                                    <strong>Chi Nhánh:</strong> Nha Khoa Việt Mỹ Quận 7 </span>
+                            </div>
+                            <a href="#">(Xem Thêm)</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row list__1">
+        <div class="col-md-6">
+            <div class="list__2">
+                <div class="title-1">
+                    <span class="icon25"></span> Danh sách điều trị
+                </div>
+                <div class="list__2__info">
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Cạo Vôi Răng - Đánh Bóng 2 Hàm</strong>
+                            </p>
+                            <p>Ngày Thực Hiện: 26/03/2018</p>
+                        </div>
+                    </div>
+                    <a href="#">(Xem Thêm)</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+            <div class="list__2">
+                <div class="title-1">
+                    <span class="icon27"></span> Chi tiết điều
+                </div>
+                <div class="list__2__info">
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon28 icon-list"></span>
+                            <p>
+                                <strong>Lịch Điều Trị</strong>
+                            </p>
+                            <p>Ngày Bắt Đầu: 9:30 AM - 26/03/2018
+                                <br/> Ngày Kết Thúc: 10:30 AM - 26/04/2018</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon29 icon-list"></span>
+                            <p>
+                                <strong>Răng Số: </strong>
+                            </p>
+                            <p>2 Hàm</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon30 icon-list"></span>
+                            <p>
+                                <strong>Triệu Chứng: </strong>
+                            </p>
+                            <p>Chảy Máu Chân Răng</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon31 icon-list"></span>
+                            <p>
+                                <strong>Chuẩn Đoán: </strong>
+                            </p>
+                            <p>Viêm Nha Chu</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon32 icon-list"></span>
+                            <p>
+                                <strong>Loại Điều Trị:</strong>
+                            </p>
+                            <p>Cạo Vôi Răng - Đánh Bóng 2 Hàm</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon33 icon-list"></span>
+                            <p>
+                                <strong>Bảo Hiểm Thanh Toán:</strong>
+                            </p>
+                            <p>5.000.000 VND</p>
+                        </div>
+                    </div>
+
+                    <a href="#">(Xem Thêm)</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="row list__5">
+        <div class="col-md-6">
+
+            <div class="list__2">
+                <div class="title-1">
+                    <span class="icon34"></span> Chi Tiết Thanh Toán
+                </div>
+                <div class="list__2__info">
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <a href="#">(Xem Thêm)</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+
+            <div class="list__2">
+                <div class="title-1">
+                    <span class="icon35"></span> Tiến Trình Điều Trị
+
+                </div>
+                <div class="list__2__info">
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <div class="list__2__des">
+                        <div class="list__2__item">
+                            <span class="icon26 icon-list"></span>
+                            <p>
+                                <strong>Thanh Toán Lần 1: </strong>
+                            </p>
+                            <p>5.000.000 VND (Ngày Thanh Toán: 26/03/2018)</p>
+                        </div>
+                    </div>
+                    <a href="#">(Xem Thêm)</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="list__3">
+        <div class="title-1">
+            <span class="icon36"></span> Hình Ảnh Điều Trị
+        </div>
+        <div class="list__3__info">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Hình Ảnh Trước Và Sau Điều Trị</p>
+                    <div class="list__3__img">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/img2.jpg">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/img2.jpg">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <p>Hình Ảnh Chụp XQuang</p>
+                    <div class="list__3__img">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/img2.jpg">
+                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/img2.jpg">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="list__4">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="title-1">
+                    <span class="icon37"></span> Bảo Hành
+                </div>
+                <div class="list__3__info">
+                    <p><strong>Thời Gian Bảo Hành</strong>: 3 Năm
+                        <br/> <strong>Ngày Bắt Đầu:</strong> 9:30 AM - 26/03/2018
+                        <br/> <strong>Ngày Kết Thúc:</strong> 10:30 AM - 26/04/2018
+                        <br/> <strong>Thời Gian Còn Lại</strong>: 365 Ngày</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="title-1">
+                    <span class="icon38"></span> Lịch Hẹn Tái Khám
+                </div>
+                <div class="list__3__info">
+                    <p><strong>Thời Gian Bảo Hành</strong>: 3 Năm
+                        <br/> <strong>Ngày Bắt Đầu:</strong> 9:30 AM - 26/03/2018
+                        <br/> <strong>Ngày Kết Thúc:</strong> 10:30 AM - 26/04/2018
+                        <br/> <strong>Thời Gian Còn Lại</strong>: 365 Ngày</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
