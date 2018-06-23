f99cc258b7f0232cb4270527ffd9155507463d38
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-06-23 15:34:47 +0700

    test change file

--- protected/modules/admin/controllers/ActionsRolesController.php
+++ protected/modules/admin/controllers/ActionsRolesController.php
@@ -2,6 +2,7 @@
 
 class ActionsRolesController extends AdminController
 {
+    public $test;
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
