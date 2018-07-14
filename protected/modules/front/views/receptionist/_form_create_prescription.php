ff7af3e20aef0f9050490e4e2cf063cdb697827c
NguyenPT <nixforest21991920@gmail.com>
2018-07-15 04:06:53 +0700

    [NguyenPT][BUG0008] Create prescription form

--- protected/modules/front/views/receptionist/_form_create_prescription.php
+++ protected/modules/front/views/receptionist/_form_create_prescription.php
@@ -57,70 +57,103 @@
             <?php echo $form->textField($customer, 'address',
                     array(
                         'readonly'  => 'readonly',
-                        'cold'      => 50,
+                        'style'      => 'width: 75%;',
                         )); ?>
         </div>
     </div>
     <div class="row">
-        <?php echo $form->labelEx($model, 'created_date'); ?>
-        <?php
-        if ($model->isNewRecord) {
-            $date = CommonProcess::getCurrentDateTime(DomainConst::DATE_FORMAT_3);
-        } else {
-            $date = CommonProcess::convertDateTime($model->created_date, DomainConst::DATE_FORMAT_1, DomainConst::DATE_FORMAT_3);
-        }
-        $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-            'model' => $model,
-            'attribute' => 'created_date',
-            'options' => array(
-                'showAnim' => 'fold',
-                'dateFormat' => DomainConst::DATE_FORMAT_2,
-                'changeMonth' => true,
-                'changeYear' => true,
-                'showOn' => 'button',
-                'buttonImage' => Yii::app()->theme->baseUrl . '/img/icon_calendar_r.gif',
-                'buttonImageOnly' => true,
-            ),
-            'htmlOptions' => array(
-                'class' => 'w-16',
-                'readonly' => 'readonly',
-                'value' => $date,
-            ),
-        ));
-        ?>
-        <?php echo $form->error($model, 'created_date'); ?>
+        <div class="col-md-6">
+            <?php echo $form->labelEx($model, 'created_date'); ?>
+            <?php
+            if ($model->isNewRecord) {
+                $date = CommonProcess::getCurrentDateTime(DomainConst::DATE_FORMAT_3);
+            } else {
+                $date = CommonProcess::convertDateTime($model->created_date, DomainConst::DATE_FORMAT_1, DomainConst::DATE_FORMAT_3);
+            }
+            $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                'model' => $model,
+                'attribute' => 'created_date',
+                'options' => array(
+                    'showAnim' => 'fold',
+                    'dateFormat' => DomainConst::DATE_FORMAT_2,
+                    'changeMonth' => true,
+                    'changeYear' => true,
+                    'showOn' => 'button',
+                    'buttonImage' => Yii::app()->theme->baseUrl . '/img/icon_calendar_r.gif',
+                    'buttonImageOnly' => true,
+                ),
+                'htmlOptions' => array(
+                    'class' => 'w-16',
+//                    'readonly' => 'readonly',
+                    'value' => $date,
+                ),
+            ));
+            ?>
+            <?php echo $form->error($model, 'created_date'); ?>
+        </div>
+        <div class="col-md-6">
+            <?php echo $form->labelEx($model, 'doctor_id'); ?>
+            <?php // echo $form->hiddenField($model, 'doctor_id', array('class' => ''));  ?>
+            <?php
+            //                    $userName = isset($model->rDoctor) ? $model->rDoctor->getAutoCompleteUserName() : '';
+            //                    $url = Yii::app()->createAbsoluteUrl('admin/ajax/searchUser',
+            //                            array('role'=> Roles::getRoleByName('ROLE_DOCTOR')->id));
+            //                    $aData = ['model'             => $model,
+            //                        'field_id'          => 'doctor_id',
+            //                        'update_value'      => $userName,
+            //                        'ClassAdd'          => 'w-350',
+            //                        'url'               => $url,
+            //                        'field_autocomplete_name' => 'autocomplete_name_doctor',
+            //                       ];
+            //                    $this->widget('ext.AutocompleteExt.AutocompleteExt',
+            //                            array('data' => $aData));
+            echo $form->dropDownList($model, 'doctor_id', Users::getListUser(
+                            Roles::getRoleByName(Roles::ROLE_DOCTOR)->id, Yii::app()->user->agent_id)
+            );
+            ?>
+            <?php echo $form->error($model, 'doctor_id'); ?>
+        </div>
     </div>
 
     <div class="row">
-<?php echo $form->labelEx($model, 'doctor_id'); ?>
-        <?php // echo $form->hiddenField($model, 'doctor_id', array('class' => ''));  ?>
-        <?php
-//                    $userName = isset($model->rDoctor) ? $model->rDoctor->getAutoCompleteUserName() : '';
-//                    $url = Yii::app()->createAbsoluteUrl('admin/ajax/searchUser',
-//                            array('role'=> Roles::getRoleByName('ROLE_DOCTOR')->id));
-//                    $aData = ['model'             => $model,
-//                        'field_id'          => 'doctor_id',
-//                        'update_value'      => $userName,
-//                        'ClassAdd'          => 'w-350',
-//                        'url'               => $url,
-//                        'field_autocomplete_name' => 'autocomplete_name_doctor',
-//                       ];
-//                    $this->widget('ext.AutocompleteExt.AutocompleteExt',
-//                            array('data' => $aData));
-        echo $form->dropDownList($model, 'doctor_id', Users::getListUser(
-                        Roles::getRoleByName(Roles::ROLE_DOCTOR)->id, Yii::app()->user->agent_id)
-        );
-        ?>
-        <?php echo $form->error($model, 'doctor_id'); ?>
+        <div class="col-md-6">
+            <?php echo $form->labelEx($model, 'note'); ?>
+            <?php echo $form->textArea($model, 'note', array('rows' => 6, 'cols' => 50)); ?>
+            <?php echo $form->error($model, 'note'); ?>
+        </div>
+        <div class="col-md-6">
+        </div>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model, 'note'); ?>
-<?php echo $form->textArea($model, 'note', array('rows' => 6, 'cols' => 50)); ?>
-<?php echo $form->error($model, 'note'); ?>
+        <div class="col-md-2">
+            Tên thuốc
+        </div>
+        <div class="col-md-1">
+            Đơn vị
+        </div>
+        <div class="col-md-1">
+            SL
+        </div>
+        <div class="col-md-1">
+            S
+        </div>
+        <div class="col-md-1">
+            T
+        </div>
+        <div class="col-md-1">
+            C
+        </div>
+        <div class="col-md-1">
+            T
+        </div>
+        <div class="col-md-1">
+            Cách dùng
+        </div>
+        <div class="col-md-3">
+            Lưu ý
+        </div>
     </div>
-
-        <?php echo DomainConst::CONTENT00081; ?>
 
     <div class="row buttons">
     <?php
