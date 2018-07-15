c9a9bc219216188a7f021b7aee20ef1277cd8bf4
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-16 01:24:36 +0700

    [NamNH][BUG0022] add action report customer

--- protected/modules/admin/controllers/ReportsController.php
+++ protected/modules/admin/controllers/ReportsController.php
@@ -186,5 +186,81 @@ class ReportsController extends AdminController
                 DomainConst::KEY_ACTIONS => $this->listActionsCanAccess,
             ));
         }
+        
+        /**
+         * report Customer
+         */
+        public function actionCustomer()
+	{
+            $dateFormat = DomainConst::DATE_FORMAT_4;
+            $agentId = isset(Yii::app()->user->agent_id) ? Yii::app()->user->agent_id : '';
+            $mAgent = Agents::model()->findByPk($agentId);
+            $from = '';
+            $to = '';
+            $old = null;
+            $new = null;
+            // Get data from url
+            $this->validateRevenueUrl($from, $to);
+            if (empty($from)) {
+                $from = CommonProcess::getCurrentDateTime(DomainConst::DATE_FORMAT_4);
+            }
+            if (empty($to)) {
+                $to = CommonProcess::getCurrentDateTime(DomainConst::DATE_FORMAT_4);
+            }
+            // Start access db
+            if ($mAgent) {
+                $data = $mAgent->getCustomers($from, $to);
+                $old = !empty($data['OLD']) ? $data['OLD'] : null;
+                $new = !empty($data['NEW']) ? $data['NEW'] : null;
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT)) {
+                $this->redirect(array('customer',
+                    'from'  => CommonProcess::convertDateTime($_POST['from_date'], DomainConst::DATE_FORMAT_BACK_END, $dateFormat),
+                    'to'    => CommonProcess::convertDateTime($_POST['to_date'], DomainConst::DATE_FORMAT_BACK_END, $dateFormat)
+                    ));
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT_MONTH)) {
+                $from = CommonProcess::getFirstDateOfCurrentMonth($dateFormat);
+                $this->redirect(array('customer',
+                    'from'  => $from,
+                    'to'    => CommonProcess::getLastDateOfMonth($from)
+                    ));
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT_LAST_MONTH)) {
+                $from = CommonProcess::getPreviousMonth($dateFormat);
+                $this->redirect(array('customer',
+                    'from'  => CommonProcess::getFirstDateOfMonth($from),
+                    'to'    => CommonProcess::getLastDateOfMonth($from)
+                    ));
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT_TODATE)) {
+                $from = CommonProcess::getCurrentDateTime($dateFormat);
+                $this->redirect(array('customer',
+                    'from'  => $from,
+                    'to'    => $from
+                    ));
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT_DATE_YESTERDAY)) {
+                $from = CommonProcess::getPreviousDateTime($dateFormat);
+                $this->redirect(array('customer',
+                    'from'  => $from,
+                    'to'    => $from
+                    ));
+            }
+            if (filter_input(INPUT_POST, DomainConst::KEY_SUBMIT_DATE_BEFORE_YESTERDAY)) {
+                $from = CommonProcess::getDateBeforeYesterdayDateTime($dateFormat);
+                $this->redirect(array('customer',
+                    'from'  => $from,
+                    'to'    => $from
+                    ));
+            }
+            $this->render('customers', array(
+                    'old'  => $old,
+                    'new'  => $new,
+                    'from'      => $from,
+                    'to'        => $to,
+                    DomainConst::KEY_ACTIONS => $this->listActionsCanAccess,
+            ));
+	}
 
 }
