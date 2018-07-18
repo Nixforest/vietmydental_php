8add458ab157a0ad76e55870546736c031240e7c
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-18 23:38:29 +0700

    [NamNH][BUG0040] add news model

---
+++ protected/modules/admin/models/News.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This is the model class for table "news".
+ *
+ * The followings are the available columns in table 'news':
+ * @property string $id
+ * @property string $content
+ * @property integer $status
+ * @property string $created_date
+ * @property string $created_by
+ */
+class News extends BaseActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return News the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'news';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('content, status', 'required'),
+			array('status', 'numerical', 'integerOnly'=>true),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+                        array('content, status,description', 'safe'),
+			array('id, content, status, created_date, created_by,description', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'content' => 'Nội dung',
+			'status' => 'Trạng thái',
+			'created_date' => 'Ngày tạo',
+			'created_by' => 'Người tạo',
+			'description' => 'Mô tả',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('content',$this->content,true);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('created_date',$this->created_date,true);
+		$criteria->compare('created_by',$this->created_by,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+        
+        /**
+         * get field of table
+         * @param string $field
+         * @return string
+         */
+        public function getField($field = 'id'){
+            return !empty($this->$field) ? $this->$field : '';
+        }
+}
