8d5ac37be95e225b91f9d55eeac92f91212cbe59
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-19 00:18:52 +0700

    [NamNH][BUG0040] add ckeditor

--- themes/ckeditor/config.js
+++ themes/ckeditor/config.js
@@ -4,37 +4,10 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
-
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+        config.removeDialogTabs = 'image:advanced;link:advanced';
         config.removePlugins = 'save,newpage,preview,print,templates,find,replace,selectall,spellchecker,forms,language,about';
         config.filebrowserBrowseUrl = '../../../../../themes/kcfinder/browse.php?opener=ckeditor&type=files';
         config.filebrowserImageBrowseUrl = '../../../../../themes/kcfinder/browse.php?opener=ckeditor&type=images';
@@ -42,5 +15,4 @@ CKEDITOR.editorConfig = function( config ) {
         config.filebrowserUploadUrl = '../../../../../themes/kcfinder/upload.php?opener=ckeditor&type=files';
         config.filebrowserImageUploadUrl = '../../../../../themes/kcfinder/upload.php?opener=ckeditor&type=images';
         config.filebrowserFlashUploadUrl = '../../../../../themes/kcfinder/upload.php?opener=ckeditor&type=flash';
-
 };
