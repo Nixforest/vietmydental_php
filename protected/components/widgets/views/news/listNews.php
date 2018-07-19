859c44f468fb19b357a6ffbd8ebdc04b7a9d55a1
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-19 16:45:09 +0700

    [NamNH][BUG0040] add widgets

---
+++ protected/components/widgets/views/news/listNews.php
@@ -0,0 +1,12 @@
+<div class="ad_read_news">
+    <section>
+        <h1 class="title"><strong>Bảng tin</strong></h1>
+        <div class="content document">
+            <ul>
+                <li>
+                    <a class="_link" href="#">Tin 1</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</div>
