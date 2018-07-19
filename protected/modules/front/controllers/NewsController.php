5236f8c5d74fe0e99a8ae05ce66fc7d49de8688a
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-19 21:38:19 +0700

    [NamNH][BUG0040] set model of view news

--- protected/modules/front/controllers/NewsController.php
+++ protected/modules/front/controllers/NewsController.php
@@ -7,8 +7,25 @@ class NewsController extends FrontController {
      * @param int $id
      */
     public function actionView($id){
-         $this->render('view', array(
+        $mNews = $this->loadModel($id);
+        $this->render('view', array(
+            'model' => $mNews,
             DomainConst::KEY_ACTIONS => $this->listActionsCanAccess,
         ));
+    }
+    
+    /**
+    * Returns the data model based on the primary key given in the GET variable.
+    * If the data model is not found, an HTTP exception will be raised.
+    * @param integer $id the ID of the model to be loaded
+    * @return News the loaded model
+    * @throws CHttpException
+    */
+    public function loadModel($id)
+    {
+        $model = News::model()->findByPk($id);
+        if($model===null)
+                throw new CHttpException(404,'The requested page does not exist.');
+        return $model;
     }
 }
