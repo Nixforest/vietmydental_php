94752740a34e50bbfacf5ea06554a0160dce5f51
hoangnam1997 <hoangnam.11.1997@gmail.com>
2018-07-19 14:42:05 +0700

    [NamNH][BUG0040] change view of s create/

--- protected/modules/admin/views/news/_search.php
+++ protected/modules/admin/views/news/_search.php
@@ -20,6 +20,11 @@
 		<?php echo $form->label($model,'content'); ?>
 		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
 	</div>
+    
+	<div class="row">
+		<?php echo $form->label($model,'description'); ?>
+		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
 
 	<div class="row">
 		<?php echo $form->label($model,'status'); ?>
