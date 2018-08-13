2327d180e01dce55b3fbd68b7f7b00d8d0d23340
NguyenPT <nixforest21991920@gmail.com>
2018-08-13 11:03:45 +0700

    [NguyenPT][BUG0056] Merge code

--- protected/modules/admin/views/treatmentScheduleDetails/updateImageReal.php
+++ protected/modules/admin/views/treatmentScheduleDetails/updateImageReal.php
@@ -2,40 +2,40 @@
 /* @var $this TreatmentScheduleDetailsController */
 /* @var $model TreatmentScheduleDetails */
 
-$this->createMenu('update', $model);
+$this->createMenu('updateImageReal', $model);
 ?>
 
-<h1><?php echo $this->pageTitle . ' ' . $model->id; ?></h1>
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
+<h1><?php echo $this->pageTitle; ?></h1>
+<?php // $this->widget('zii.widgets.CDetailView', array(
+//	'data'=>$model,
+//	'attributes'=>array(
 //		'id',
-		'schedule_id',
-		'start_date',
-		'end_date',
-		array(
-                    'label' => DomainConst::CONTENT00284,
-                    'type'=>'html',
-                    'value'=> isset($model->rJoinTeeth) ? $model->generateTeethInfo() : '',
-                ),
-		array(
-                   'name'=>'diagnosis_id',
-                   'value'=> isset($model->rDiagnosis) ? $model->rDiagnosis->name : '',
-                ),
-		array(
-                   'name'=>'treatment_type_id',
-                   'value'=> isset($model->rTreatmentType) ? $model->rTreatmentType->name : '',
-                ),
-		'description',
-		'type_schedule',
-                array(
-                    'name'  => DomainConst::CONTENT00026,
-                    'htmlOptions' => array('style' => 'text-align:center;'),
-                    'value' => TreatmentScheduleDetails::getStatus()[$model->status],
-                    'visible' => CommonProcess::isUserAdmin(),
-                ),
-	),
-)); ?>
+//		'schedule_id',
+//		'start_date',
+//		'end_date',
+//		array(
+//                    'label' => DomainConst::CONTENT00284,
+//                    'type'=>'html',
+//                    'value'=> isset($model->rJoinTeeth) ? $model->generateTeethInfo() : '',
+//                ),
+//		array(
+//                   'name'=>'diagnosis_id',
+//                   'value'=> isset($model->rDiagnosis) ? $model->rDiagnosis->name : '',
+//                ),
+//		array(
+//                   'name'=>'treatment_type_id',
+//                   'value'=> isset($model->rTreatmentType) ? $model->rTreatmentType->name : '',
+//                ),
+//		'description',
+//		'type_schedule',
+//                array(
+//                    'name'  => DomainConst::CONTENT00026,
+//                    'htmlOptions' => array('style' => 'text-align:center;'),
+//                    'value' => TreatmentScheduleDetails::getStatus()[$model->status],
+//                    'visible' => CommonProcess::isUserAdmin(),
+//                ),
+//	),
+//)); ?>
 <div class="form">
     <?php
     $form = $this->beginWidget(
